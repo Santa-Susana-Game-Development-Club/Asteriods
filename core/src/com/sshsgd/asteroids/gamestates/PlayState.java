3ee671c0bd0075582b0790cdd745dc3e31583a3f
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-01-31 12:46:03 -0800

    Collision

--- core/src/com/sshsgd/asteroids/gamestates/PlayState.java
+++ core/src/com/sshsgd/asteroids/gamestates/PlayState.java
@@ -19,10 +19,14 @@ public class PlayState extends GameState {
 
 	private MyCamera cam;
 	private Viewport view;
+	private Player player;
 	private Array<Asteroid> asteroids;
 	private Array<Particles> particles;
+	private Array<Bullet> bullets;
 	
-	private Player p;
+	private int level;
+	
+	private float deadTime;
 	
 	public PlayState(GameStateManager gsm) {
 		super(gsm);
@@ -34,46 +38,67 @@ public class PlayState extends GameState {
 		view = new StretchViewport(MyConstants.WOLRD_WIDTH, MyConstants.WORLD_HEIGHT, cam);
 		view.apply(true);
 		view.update((int)Game.SIZE.x, (int)Game.SIZE.y, true);
-		p = new Player();
+		player = new Player();
 		asteroids = new Array<Asteroid>();
 		particles = new Array<Particles>();
-		asteroids.add(new Asteroid(MathUtils.random(MyConstants.WOLRD_WIDTH), MathUtils.random(MyConstants.WORLD_HEIGHT), Size.Large));
+		bullets = new Array<Bullet>();
+		
+		level = 0;
+		
+		deadTime = 0;
 	}
 
 	@Override
 	public void handleInput() {
-		p.handleInput();
+		if(player.isAlive())player.handleInput();
 		if(MyInput.keyPressed(MyInput.SHOOT)) {
-			if(asteroids.size > 0) {
-				float tempX = asteroids.get(0).getX();
-				float tempY = asteroids.get(0).getY();
-				switch (asteroids.get(0).getSize()) {
-				case Large:
-					asteroids.add(new Asteroid(tempX, tempY, Size.Medium));
-					asteroids.add(new Asteroid(tempX, tempY, Size.Medium));
-					break;
-				case Medium:
-					asteroids.add(new Asteroid(tempX, tempY, Size.Small));
-					asteroids.add(new Asteroid(tempX, tempY, Size.Small));
-					break;
-				case Small:
-					particles.add(new Particles(asteroids.get(0)));
-					break;
-				}
-				asteroids.get(0).dispose();
-				asteroids.removeIndex(0);
+			if(player.isAlive() && bullets.size < 5) {
+				bullets.add(new Bullet(player));
 			}
-		}
-		if(MyInput.keyPressed(MyInput.START)) {
-			particles.add(new Particles(p));
 		}
 	}
 
 	@Override
 	public void update(float dt) {
-		p.update(dt);
+		if(asteroids.size < 1) {
+			level++;
+			createAsteroids();
+		}
+		if(player.isAlive()) {
+			deadTime = 0;
+			player.update(dt);
+		} else {
+			deadTime += dt;
+			if(deadTime > 1) {
+				player.setPos(MyConstants.WOLRD_WIDTH * .5f, MyConstants.WORLD_HEIGHT * .5f);
+				player.setRadians(0);
+				player.setSpeed(0);
+				player.update(0);
+				player.setAlive(true);
+			}
+		}
 		for (Asteroid asteroid : asteroids) {
 			asteroid.update(dt);
+			if(player.isAlive() && asteroid.intersects(player)) {
+				killPlayer();
+				destroyAsteroid(asteroid);
+			}
+		}
+		for (Bullet bullet : bullets) {
+			bullet.update(dt);
+			if(bullet.isShouldRemove()) {
+				bullets.removeValue(bullet, true);
+				continue;
+			}
+		}
+		for (Asteroid asteroid : asteroids) {
+			for (Bullet bullet : bullets) {
+				if(asteroid.contains(bullet.getPos())) {
+					destroyAsteroid(asteroid);
+					bullets.removeValue(bullet, true);
+					continue;
+				}
+			}
 		}
 	}
 
@@ -81,16 +106,21 @@ public class PlayState extends GameState {
 	public void draw(SpriteBatch sb, ShapeRenderer sr, float dt) {
 		sr.begin(ShapeType.Line);
 		sr.setProjectionMatrix(cam.combined);
-		p.draw(sb, sr, dt);
+		if(player.isAlive())player.draw(sb, sr, dt);
 		for (Asteroid asteroid : asteroids) {
 			asteroid.draw(sb, sr, dt);
 		}
+		sr.end();
+		sr.begin(ShapeType.Filled);
 		for (Particles particle : particles) {
 			particle.draw(sr, dt);
 			if(particle.isShouldRemove()) {
 				particles.removeValue(particle, true);
 				break;
 			}
+		}
+		for (Bullet bullet : bullets) {
+			bullet.draw(sr);
 		}
 		sr.end();
 	}
@@ -104,6 +134,54 @@ public class PlayState extends GameState {
 	public void dispose() {
 		particles.clear();
 		asteroids.clear();
+	}
+	
+	private void killPlayer() {
+		particles.add(new Particles(player));
+		player.setAlive(false);
+		player.setPos(MyConstants.WOLRD_WIDTH * .5f, MyConstants.WORLD_HEIGHT * .5f);
+		player.setRadians(0);
+		player.setSpeed(0);
+	}
+	
+	private void destroyAsteroid(Asteroid asteroid) {
+		if(asteroids.size > 0) {
+			float tempX = asteroid.getX();
+			float tempY = asteroid.getY();
+			switch (asteroid.getSize()) {
+			case Large:
+				asteroids.add(new Asteroid(tempX, tempY, Size.Medium));
+				asteroids.add(new Asteroid(tempX, tempY, Size.Medium));
+				break;
+			case Medium:
+				asteroids.add(new Asteroid(tempX, tempY, Size.Small));
+				asteroids.add(new Asteroid(tempX, tempY, Size.Small));
+				break;
+			case Small:
+				particles.add(new Particles(asteroid));
+				break;
+			}
+			asteroid.dispose();
+			asteroids.removeValue(asteroid, true);
+		}
+	}
+	
+	private void createAsteroids() {
+		int numToSpawn = 4 + level - 1;
+		for(int i = 0; i < numToSpawn; i++) {
+			float x, y, dx, dy, dist;
+			do {
+				 x = MathUtils.random(MyConstants.WOLRD_WIDTH);
+				 y = MathUtils.random(MyConstants.WORLD_HEIGHT);
+				 
+				 dx = x - player.getX();
+				 dy = y - player.getY();
+				 
+				 dist = (float) Math.sqrt(dx * dx + dy * dy);
+				 
+			} while(dist < 175);
+			asteroids.add(new Asteroid(x, y, Size.Large));
+		}
 	}
 
 }
